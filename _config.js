4aa8385f43782fb6e3ef7972e4c891dd2bf3471e
lthuita <lthuita@gmail.com>
2020-08-25 14:34:02 +0300

    modified the mongodb URI

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://lthuita:Lumathu1983.@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://lthuita:Lumathu1983.@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://lthuita:Lumathu1983.@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://lthuita:Lumathu1983.@cluster0.ysvco.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://lthuita:Lumathu1983.@cluster0.ysvco.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://lthuita:Lumathu1983.@cluster0.ysvco.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
