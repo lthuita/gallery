bb04bc2192d333105ad5a899235c1284ef69df96
lthuita <lthuita@gmail.com>
2020-08-25 16:20:01 +0300

    troubleshooting on the db connections

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://lthuita:Lumathu1983.@lunicluster1.mou1f.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://lthuita:Lumathu1983.@lunicluster1.mou1f.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://lthuita:Lumathu1983.@lunicluster1.mou1f.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://new_user:new_user@lunicluster1.mou1f.mongodb.net/dark?retryWrites=true&w=majority',
+    development: 'mongodb+srv://new_user:new_user@lunicluster1.mou1f.mongodb.net/dark-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://new_user:new_user@lunicluster1.mou1f.mongodb.net/dark-test?retryWrites=true&w=majority',
 }
 module.exports = config;
